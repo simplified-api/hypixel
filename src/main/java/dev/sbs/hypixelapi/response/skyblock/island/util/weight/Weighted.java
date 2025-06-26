3d661cc083b4840f8bffc6942e016066b16cc9d6
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2025-06-26 03:36:13 -0400

    Moved to simplified-bot project

--- src/main/java/dev/sbs/hypixelapi/response/skyblock/island/util/weight/Weighted.java
+++
@@ -1,9 +0,0 @@
-package dev.sbs.minecraftapi.client.hypixel.response.skyblock.island.util.weight;
-
-import org.jetbrains.annotations.NotNull;
-
-public interface Weighted {
-
-    @NotNull Weight getWeight();
-
-}
