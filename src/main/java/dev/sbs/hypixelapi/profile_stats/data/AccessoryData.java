e13d01f3be30948b5abb7c7ab2622a68bed42160
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2026-04-27 00:35:21 -0400

    Incomplete profile stats data classes, don't want to lose them

---
+++ src/main/java/dev/sbs/hypixelapi/profile_stats/data/AccessoryData.java
@@ -0,0 +1,170 @@
+package dev.sbs.hypixelapi.profile_stats.data;
+
+import dev.sbs.skyblockdata.SkyBlockData;
+import dev.sbs.skyblockdata.common.Rarity;
+import dev.sbs.skyblockdata.model.Accessory;
+import dev.sbs.skyblockdata.model.BonusItemStat;
+import dev.sbs.skyblockdata.model.Stat;
+import dev.simplified.collection.Concurrent;
+import dev.simplified.collection.ConcurrentList;
+import dev.simplified.collection.ConcurrentMap;
+import lib.minecraft.nbt.NbtFactory;
+import lib.minecraft.nbt.exception.NbtException;
+import lib.minecraft.nbt.tags.array.ByteArrayTag;
+import lib.minecraft.nbt.tags.collection.CompoundTag;
+import lib.minecraft.nbt.tags.collection.ListTag;
+import lib.minecraft.nbt.tags.primitive.IntTag;
+import lib.minecraft.nbt.tags.primitive.StringTag;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Objects;
+import java.util.Optional;
+
+@Getter
+public class AccessoryData extends ObjectData<AccessoryData.Type> {
+
+    private static final ConcurrentList<Integer> PULSE_CHARGES = Concurrent.newList(150_000, 1_000_000, 5_000_000);
+    private final Accessory accessory;
+    private boolean bonusCalculated;
+    private final Optional<Stat> enrichmentStat;
+
+    public AccessoryData(Accessory accessory, CompoundTag compoundTag) {
+        super(accessory.getItem(), compoundTag);
+        this.accessory = accessory;
+
+        // Load Enrichment - look up stat by enrichment key from NBT
+        String enrichmentKey = compoundTag.getPathOrDefault("tag.ExtraAttributes.talisman_enrichment", StringTag.EMPTY).getValue().toUpperCase();
+        this.enrichmentStat = SkyBlockData.getRepository(Stat.class)
+            .findFirst(Stat::getId, enrichmentKey);
+
+        // Handle Gemstone Stats
+        PlayerDataHelper.handleGemstoneBonus(this)
+            .forEach((statModel, value) -> this.addBonus(this.getStats(AccessoryData.Type.GEMSTONES).get(statModel), value));
+
+        // Handle Stats
+        this.getAccessory().getItem().getStats().forEach((key, value) -> SkyBlockData.getRepository(Stat.class).findFirst(Stat::getId, key)
+            .ifPresent(statModel -> this.addBonus(this.getStats(AccessoryData.Type.STATS).get(statModel), value)));
+
+        // Handle Enrichment Stats
+        this.getEnrichmentStat()
+            .filter(stat -> stat.getEnrichment() > 0.0)
+            .ifPresent(stat -> this.addBonus(this.getStats(AccessoryData.Type.ENRICHMENTS).get(stat), stat.getEnrichment()));
+
+        // New Year Cake Bag
+        if ("NEW_YEAR_CAKE_BAG".equals(this.getAccessory().getItem().getId())) {
+            try {
+                byte[] nbtCakeBag = compoundTag.getPathOrDefault("tag.ExtraAttributes.new_year_cake_bag_data", ByteArrayTag.EMPTY).getValue();
+                ListTag<CompoundTag> cakeBagItems = NbtFactory.fromByteArray(nbtCakeBag).getListTag("i");
+                SkyBlockData.getRepository(Stat.class).findFirst(Stat::getId, "HEALTH")
+                    .ifPresent(statModel -> this.addBonus(this.getStats(AccessoryData.Type.CAKE_BAG).get(statModel), cakeBagItems.size()));
+            } catch (NbtException ignore) { }
+        }
+    }
+
+    @Override
+    protected int handleRarityUpgrades(int rarityOrdinal) {
+        int increaseRarity = 0;
+
+        if (this.getItem().getId().equals("POWER_ARTIFACT")) {
+            long perfects = this.getGemstones()
+                .stream()
+                .flatMap(entry -> entry.getValue().stream())
+                .filter(gemstoneType -> gemstoneType.name().equals("PERFECT"))
+                .count();
+
+            increaseRarity = (perfects == 7) ? 1 : 0;
+        }
+
+        if (this.getItem().getId().equals("PANDORAS_BOX")) {
+            String pandoraRarityKey = super.getCompoundTag().getPathOrDefault("tag.ExtraAttributes.pandora-rarity", StringTag.EMPTY).getValue().toUpperCase();
+            try {
+                Rarity pandoraRarity = Rarity.valueOf(pandoraRarityKey);
+                increaseRarity = pandoraRarity.ordinal() - rarityOrdinal;
+            } catch (IllegalArgumentException ignore) { }
+        }
+
+        if (this.getItem().getId().equals("PULSE_RING")) {
+            int thunderCharge = this.getCompoundTag().getPathOrDefault("tag.ExtraAttributes.thunder_charge", IntTag.EMPTY).getValue();
+
+            for (int i = 0; i < PULSE_CHARGES.size(); i++) {
+                if (thunderCharge >= PULSE_CHARGES.get(i))
+                    increaseRarity++;
+            }
+        }
+
+        if (this.getItem().getId().equals("TRAPPER_CREST")) {
+            int pelts = this.getCompoundTag().getPathOrDefault("tag.ExtraAttributes.pelts_earned", IntTag.EMPTY).getValue();
+            increaseRarity = (pelts >= 500) ? 1 : 0;
+        }
+
+        return rarityOrdinal + increaseRarity;
+    }
+
+    @Override
+    public AccessoryData calculateBonus(ConcurrentMap<String, Double> expressionVariables) {
+        if (!this.isBonusCalculated()) {
+            this.bonusCalculated = true;
+
+            // Handle Bonus Item Stats
+            this.getBonusItemStatModels()
+                .stream()
+                .filter(BonusItemStat::noRequiredMobType)
+                .forEach(bonusItemStat -> {
+                    // Handle Bonus Gemstone Stats
+                    if (bonusItemStat.isForGems()) {
+                        this.getStats(AccessoryData.Type.GEMSTONES)
+                            .forEach((statModel, statData) -> statData.bonus = PlayerDataHelper.handleBonusEffects(statModel, statData.getBonus(), this.getCompoundTag(), expressionVariables, bonusItemStat));
+                    }
+
+                    // Handle Bonus Stats
+                    if (bonusItemStat.isForStats()) {
+                        this.getStats(AccessoryData.Type.STATS)
+                            .forEach((statModel, statData) -> statData.bonus = PlayerDataHelper.handleBonusEffects(statModel, statData.getBonus(), this.getCompoundTag(), expressionVariables, bonusItemStat));
+                    }
+                });
+        }
+
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+
+        AccessoryData that = (AccessoryData) o;
+
+        return this.isBonusCalculated() == that.isBonusCalculated()
+            && Objects.equals(this.getAccessory(), that.getAccessory());
+    }
+
+    @Override
+    protected Type[] getAllTypes() {
+        return AccessoryData.Type.values();
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * super.hashCode() + Objects.hash(this.getAccessory(), this.isBonusCalculated());
+    }
+
+    public final boolean isMissingEnrichment() {
+        return this.getRarity().isEnrichable() && this.getEnrichmentStat().isEmpty();
+    }
+
+    @Getter
+    @RequiredArgsConstructor
+    public enum Type implements ObjectData.Type {
+
+        CAKE_BAG(true),
+        GEMSTONES(true),
+        STATS(true),
+        ENRICHMENTS(true);
+
+        private final boolean optimizerConstant;
+
+    }
+
+}
---
+++ src/main/java/dev/sbs/hypixelapi/profile_stats/data/ObjectData.java
@@ -0,0 +1,177 @@
+package dev.sbs.hypixelapi.profile_stats.data;
+
+import dev.sbs.skyblockdata.SkyBlockData;
+import dev.sbs.skyblockdata.common.Rarity;
+import dev.sbs.skyblockdata.model.BonusItemStat;
+import dev.sbs.skyblockdata.model.Gemstone;
+import dev.sbs.skyblockdata.model.Item;
+import dev.sbs.skyblockdata.model.Reforge;
+import dev.sbs.skyblockdata.model.Stat;
+import dev.simplified.collection.Concurrent;
+import dev.simplified.collection.ConcurrentList;
+import dev.simplified.collection.ConcurrentMap;
+import dev.simplified.util.StringUtil;
+import lib.minecraft.nbt.tags.Tag;
+import lib.minecraft.nbt.tags.collection.CompoundTag;
+import lib.minecraft.nbt.tags.primitive.IntTag;
+import lib.minecraft.nbt.tags.primitive.StringTag;
+import lombok.Getter;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Locale;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+
+public abstract class ObjectData<T extends ObjectData.Type> extends StatData<T> {
+
+    private static final DateTimeFormatter TIMESTAMP_FORMAT = DateTimeFormatter.ofPattern("M/d/yy h:m a", Locale.US);
+    private static final ZoneId HYPIXEL_TIMEZONE = ZoneId.of("America/New_York");
+    @Getter private final Item item;
+    @Getter private final CompoundTag compoundTag;
+    @Getter private final Rarity rarity;
+    @Getter private final ConcurrentList<BonusItemStat> bonusItemStatModels;
+    @Getter private final Optional<Reforge> reforge;
+    @Getter private final ConcurrentMap<Gemstone, ConcurrentList<Gemstone.Type>> gemstones;
+    @Getter private final boolean recombobulated;
+    @Getter private final boolean tierBoosted;
+    @Getter private final Optional<Long> timestamp;
+
+    protected ObjectData(Item itemModel, CompoundTag compoundTag) {
+        this.item = itemModel;
+        this.compoundTag = compoundTag;
+
+        // Load Timestamp
+        this.timestamp = Optional.ofNullable(
+                StringUtil.defaultIfEmpty(
+                    compoundTag.getPathOrDefault("tag.ExtraAttributes.timestamp", StringTag.EMPTY).getValue(),
+                    null
+                )
+            )
+            .map(timestamp -> LocalDateTime.parse(timestamp, TIMESTAMP_FORMAT))
+            .map(localDateTime -> localDateTime.atZone(HYPIXEL_TIMEZONE))
+            .map(ZonedDateTime::toInstant)
+            .map(Instant::toEpochMilli);
+
+        // Load Recombobulator
+        this.recombobulated = compoundTag.getPathOrDefault("tag.ExtraAttributes.rarity_upgrades", IntTag.EMPTY).getValue() == 1;
+
+        // Load Tier Boost
+        this.tierBoosted = compoundTag.getPathOrDefault("tag.ExtraAttributes.baseStatBoostPercentage", IntTag.EMPTY).getValue() > 0;
+
+        // Load Gemstones
+        CompoundTag gemTag = compoundTag.getPathOrDefault("tag.ExtraAttributes.gems", CompoundTag.EMPTY);
+        this.gemstones = Concurrent.newUnmodifiableMap(gemTag.notEmpty() ? findGemstones(gemTag) : Concurrent.newMap());
+
+        // Initialize Stats
+        ConcurrentList<Stat> statModels = SkyBlockData.getRepository(Stat.class).findAll();
+        Arrays.stream(this.getAllTypes()).forEach(type -> {
+            this.stats.put(type, Concurrent.newLinkedMap());
+            statModels.forEach(statModel -> this.stats.get(type).put(statModel, new Data()));
+        });
+
+        // Load Bonus Item Stat Model
+        this.bonusItemStatModels = SkyBlockData.getRepository(BonusItemStat.class)
+            .findAll(BonusItemStat::getItemId, itemModel.getId())
+            .collect(Concurrent.toUnmodifiableList());
+
+        // Load Reforge Model
+        this.reforge = SkyBlockData.getRepository(Reforge.class)
+            .findFirst(Reforge::getId, this.getCompoundTag()
+                .getPathOrDefault("tag.ExtraAttributes.modifier", StringTag.EMPTY)
+                .getValue()
+                .toUpperCase()
+            );
+
+        // Load Rarity
+        this.rarity = Rarity.of(this.handleRarityUpgrades(
+            itemModel.getRarity().ordinal() +
+                (this.isRecombobulated() ? 1 : 0) +
+                (this.isTierBoosted() ? 1 : 0)
+        ));
+    }
+
+    public abstract ObjectData<T> calculateBonus(ConcurrentMap<String, Double> expressionVariables);
+
+    private static ConcurrentMap<Gemstone, ConcurrentList<Gemstone.Type>> findGemstones(CompoundTag gemTag) {
+        ConcurrentList<Gemstone> gemstoneModels = SkyBlockData.getRepository(Gemstone.class).findAll();
+        ConcurrentMap<Gemstone, ConcurrentList<Gemstone.Type>> gemstones = Concurrent.newMap();
+
+        for (Map.Entry<String, Tag<?>> entry : gemTag.entrySet()) {
+            for (Gemstone gemstone : gemstoneModels) {
+                boolean handle = false;
+                String typeKey = null;
+
+                // Handle Specific Slots
+                if (entry.getKey().startsWith(gemstone.getId())) {
+                    handle = true;
+                    typeKey = ((StringTag) entry.getValue()).getValue();
+                }
+
+                // Handle Generic Slots
+                if (entry.getValue().getValue().equals(gemstone.getId()) && entry.getKey().endsWith("_gem")) {
+                    handle = true;
+                    typeKey = gemTag.getOrDefault(entry.getKey().replace("_gem", ""), StringTag.EMPTY).getValue();
+                }
+
+                if (handle && typeKey != null) {
+                    // Populate New Gemstone
+                    gemstones.putIfAbsent(gemstone, Concurrent.newList());
+
+                    // Add Gemstone Type
+                    try {
+                        Gemstone.Type gemType = Gemstone.Type.valueOf(typeKey);
+                        gemstones.get(gemstone).add(gemType);
+                    } catch (IllegalArgumentException ignore) { }
+                }
+            }
+        }
+
+        return gemstones;
+    }
+
+    protected int handleRarityUpgrades(int rarityOrdinal) {
+        return rarityOrdinal;
+    }
+
+    public abstract boolean isBonusCalculated();
+
+    public final boolean notRecombobulated() {
+        return !this.isRecombobulated();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        ObjectData<?> that = (ObjectData<?>) o;
+
+        return this.isRecombobulated() == that.isRecombobulated()
+            && this.isTierBoosted() == that.isTierBoosted()
+            && Objects.equals(this.getItem(), that.getItem())
+            && Objects.equals(this.getCompoundTag(), that.getCompoundTag())
+            && Objects.equals(this.getRarity(), that.getRarity())
+            && Objects.equals(this.getBonusItemStatModels(), that.getBonusItemStatModels())
+            && Objects.equals(this.getReforge(), that.getReforge())
+            && Objects.equals(this.getGemstones(), that.getGemstones())
+            && Objects.equals(this.getTimestamp(), that.getTimestamp());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.getItem(), this.getCompoundTag(), this.getRarity(), this.getBonusItemStatModels(), this.getReforge(), this.getGemstones(), this.isRecombobulated(), this.isTierBoosted(), this.getTimestamp());
+    }
+
+    public interface Type {
+
+        boolean isOptimizerConstant();
+
+    }
+
+}
