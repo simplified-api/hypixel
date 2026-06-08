1efe6802e54906fa7affd8e17081553a104cbbb6
CraftedFury <6407029+CraftedFury@users.noreply.github.com>
2026-06-08 10:00:40 -0400

    Pin skyblock to 26284e2, gson-extras to b68510e, persistence to 8e77738, client to 64ae978

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,15 +35,15 @@ dependencies {
     testImplementation(libs.junit.platform.launcher)
 
     // Sibling API modules (composite-build substitutes by project name)
-    api("com.github.simplified-api:skyblock") { version { strictly("570edd4") } }
+    api("com.github.simplified-api:skyblock") { version { strictly("26284e2") } }
 
     // Simplified Libraries (github.com/simplified-dev)
     api("com.github.simplified-dev:collections") { version { strictly("2f2aa58") } }
     api("com.github.simplified-dev:utils") { version { strictly("a2f3ccd") } }
     api("com.github.simplified-dev:reflection") { version { strictly("c02511a") } }
-    api("com.github.simplified-dev:gson-extras") { version { strictly("26278a3") } }
-    api("com.github.simplified-dev:persistence") { version { strictly("533206b") } }
-    api("com.github.simplified-dev:client") { version { strictly("47d3c2f") } }
+    api("com.github.simplified-dev:gson-extras") { version { strictly("b68510e") } }
+    api("com.github.simplified-dev:persistence") { version { strictly("8e77738") } }
+    api("com.github.simplified-dev:client") { version { strictly("64ae978") } }
     api("com.github.simplified-dev:expression") { version { strictly("b8cfe1f") } }
 
     // Minecraft-Library (github.com/minecraft-library)
